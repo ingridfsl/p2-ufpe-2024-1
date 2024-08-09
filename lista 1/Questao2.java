b47d7105833ee24a0d1783efa1be77b49c016106
Ingrid Freire <100882928+ingridfsl@users.noreply.github.com>
2024-08-09 18:54:39 -0300

    update lista 1

--- lista 1/Questao2.java
+++ lista 1/Questao2.java
@@ -6,12 +6,12 @@ public class Questao2  {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int counter=0;
-        double soma=0;
+        int counter= 0;
+        double soma= 0;
 
         System.out.println("Informe três notas, em sequência: ");
         while(counter <=2){
-            double nota = sc.nextInt();
+            double nota = sc.nextDouble();
             soma +=nota;
             counter++;
 
